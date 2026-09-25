da9f636b17e5ef0414369eec040569ec15fac139
Aaliyan Hussain <aaliyanhussainrajput786@gmail.com>
2026-09-25 11:49:09 +0500

    Add door locking condition

---
+++ opendoor.py
@@ -0,0 +1,5 @@
+Wallet = ["Cash", "Lighter", "Pocket Knife", "Basement Keys"]
+if "Basement Keys" in Wallet:
+    print("The door unlocks with a strange sense of smell")
+else:
+    print("The door is rusted. Seems to need some kind of key")
